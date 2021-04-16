58f0f4591c5ee4ed64864c3fdf8c303ad7768461
Антон Редкозубов <uacreez@gmail.com>
2021-04-16 10:17:37 +0300

    Сделал проверку геолокации и алерт на включение

--- SimpleMap/ViewController.swift
+++ SimpleMap/ViewController.swift
@@ -6,14 +6,55 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        checkLocationEnable()
+    }
+    
+    func checkLocationEnable() {
+        if CLLocationManager.locationServicesEnabled(){
+            setupManager()
+        } else {
+            
+            let alert = UIAlertController(title: "У вас выключена геолокация", message: "Хотите включить", preferredStyle: .alert)
+            
+            let settingsAction = UIAlertAction(title: "Настройки", style: .default) { (alert) in
+                if let url = URL(string: "App-Prefs:root=LOCATION_SERVICES"){
+                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                }
+            }
+            
+            let cancelAction = UIAlertAction(title: "Отмена", style: .cancel)
+            
+            
+            alert.addAction(settingsAction)
+            alert.addAction(cancelAction)
+            
+            present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func setupManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
     }
 
 
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    
+}
